0e6bc500d32780c8d6592cf7479879974876d8d2
Emin Mastizada <emin@mastizada.com>
2020-04-25 19:08:36 +0300

    Add new html entry for the product details

--- product/views.py
+++ product/views.py
@@ -6,7 +6,7 @@ from product.models import Product, StatusChoices
 
 
 class ProductDetailsView(TemplateView):
-    template_name = "product_details.html"
+    template_name = "product/details.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
