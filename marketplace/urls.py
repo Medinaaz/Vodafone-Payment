5f4995e3e105a2322183f329180384df75b0db76
Emin Mastizada <emin@mastizada.com>
2020-04-25 19:07:50 +0300

    Enable media files if the Debug mode is enabled

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import (
-    path,
-    include,
-)
+from django.urls import path, include
+from django.views.static import serve
 
 from core.views import HomePageView
 from product.views import ProductDetailsView
@@ -13,3 +12,11 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('', HomePageView.as_view(), name="homepage"),
 ]
+
+if settings.DEBUG:
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns += [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ]
+    urlpatterns += [path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT})]
