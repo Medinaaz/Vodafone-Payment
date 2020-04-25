728c4634bed58cb8bf51d039de23efe806f4f1c7
Emin Mastizada <emin@linux.com>
2020-04-26 01:12:45 +0300

    Add sub-navigation with product categories

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -39,6 +39,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'basket.context_processors.current_basket',
+                'product.context_processors.product_categories',
             ],
         },
     },
