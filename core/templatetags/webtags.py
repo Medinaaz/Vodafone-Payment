fda9061856949aeb76cea30e65bba201e54e91ec
Emin Mastizada <emin@linux.com>
2020-05-20 00:09:23 +0300

    Test showing selected choice for the form

--- core/templatetags/webtags.py
+++ core/templatetags/webtags.py
@@ -15,3 +15,8 @@ def replace_language(path: str, lang: str) -> str:
     if not language:
         return path
     return path.replace(language, lang, 1)
+
+
+@register.filter
+def selected_choice(form, field_name):
+    return dict(form.fields[field_name].choices)[form.data[field_name]]
