f243a0d9a5acf078a66279d2daa5d95ff928e145
meg-i <isallari.megi@gmail.com>
2020-05-19 02:36:18 +0200

    Add mail host and password

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -106,6 +106,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL.strip("/"))
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'marketplace.vodafone@gmail.com'
+EMAIL_HOST_PASSWORD = 'xae127777'
+
 try:
     from local_settings import *  # NOQA
 except ImportError as e:
