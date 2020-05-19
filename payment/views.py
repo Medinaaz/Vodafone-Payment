7a7ef1d6c7cbda876693824f9c525016d663d164
Emin Mastizada <emin@linux.com>
2020-05-19 20:57:05 +0000

    Payment update

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.template import loader
+from django.views.generic import TemplateView, View
 from basket.models import Basket, BasketItem
 from shipment.models import Shipment
+from django.shortcuts import render
+from marketplace.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from shipment.models import Shipment
+
 # Create your views here.
 
 class PaymentView(TemplateView):
@@ -26,5 +34,45 @@ class PaymentView(TemplateView):
         context['total'] = total
 
         context['discount'] = total - subtotal
+        shipment = Shipment.objects.filter(user=self.request.user).values()
+        for element in shipment:
+            if element['selection']== 2 or element['selection']== 3:
+                context['name'] = element['name']
+                context['surname'] = element['surname']
+                context['email'] = element['email']
+                context['neighborhood'] = element['neighborhood']
+                context['district'] = element['district']
+                context['city'] = element['city']
 
         return context
+
+
+def mail_view(request):
+    if request.method == 'POST':
+        recipient = request.POST.get('email')
+        domain = request.get_host()
+        print(domain)
+        basket = Basket.current_basket(user=request.user)
+
+        basket_items = BasketItem.objects.filter(
+            basket=basket,
+        )
+        subtotal = sum(basket_item.product.price for basket_item in basket_items)
+        total = 0
+        if basket:
+            total = basket.coupon_code.get_discounted_value(subtotal) if basket.coupon_code else subtotal
+        discount = total - subtotal
+        subject = 'Your order has been received'
+        shipment = Shipment.objects.filter(user=request.user).values()
+        for element in shipment:
+            Address_name = element['name']
+            Address_surname = element['surname']
+            Address_email = element['email']
+            Address_neighborhood = element['neighborhood']
+            Address_district = element['district']
+            Address_city = element['city']
+        html_message = render_to_string('payment/mail_template.html', {'user': request.user, 'basket_items':basket_items, 'subtotal':subtotal, 'total':total, 'discount':discount,'neighborhood':Address_neighborhood, 'district':Address_district, 'city':Address_city, 'name':Address_name, 'surname':Address_surname})
+        plain_message = strip_tags(html_message)
+        send_mail(subject,
+            plain_message, EMAIL_HOST_USER, [recipient], html_message=html_message)
+    return render(request, 'payment/modal.html', {'total': total})
