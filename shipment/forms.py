901dc7b2e3e84cca5a726cf6b508572ca34ec5f6
Emin Mastizada <emin@linux.com>
2020-05-20 09:06:00 +0300

    add placeholder for the email fields in shipment form

--- shipment/forms.py
+++ shipment/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from shipment.models import Shipment
 
 
 class ShipmentForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": _("Email")}))
+
     class Meta:
         model = Shipment
         fields = ["address_id", "address_name", "name", "surname", "email", "phone",
